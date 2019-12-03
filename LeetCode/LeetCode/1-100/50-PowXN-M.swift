384ec27e0e3c0d6b830890939721ad2e29f0da90
yinlong <469891897@qq.com>
2019-12-03 22:53:32 +0800

    50. Pow(x, n)

---
+++ LeetCode/LeetCode/1-100/50-PowXN-M.swift
@@ -0,0 +1,63 @@
+//
+//  50-PowXN-M.swift
+//  LeetCode
+//
+//  Created by 张银龙 on 2019/12/3.
+//  Copyright © 2019 张银龙. All rights reserved.
+//
+
+import Cocoa
+
+/*
+ 50. Pow(x, n)
+ 
+ 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+ 示例 1:
+
+ 输入: 2.00000, 10
+ 输出: 1024.00000
+ 示例 2:
+
+ 输入: 2.10000, 3
+ 输出: 9.26100
+ 示例 3:
+
+ 输入: 2.00000, -2
+ 输出: 0.25000
+ 解释: 2-2 = 1/22 = 1/4 = 0.25
+ 说明:
+
+ -100.0 < x < 100.0
+ n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+
+ 链接：https://leetcode-cn.com/problems/powx-n
+ */
+
+class PowXN: NSObject {
+    func myPow(_ x: Double, _ n: Int) -> Double {
+        
+        var n = n
+        var x = x
+        
+        if n < 0 {
+            n = -n;
+            x = 1 / x;
+        }
+        
+        var pow : Double = 1.0;
+        
+        while n > 0 {
+            
+            // O(logN)
+            if n & 1 == 1{ // 奇数
+                pow = pow * x;
+            }
+            
+            x *= x;
+            n = n / 2;
+        }
+        
+        return pow;
+    }
+}
