93c7517b04e708a9058f2ab60f7b896ea1922fdf
yinlong <469891897@qq.com>
2020-02-23 21:21:15 +0800

    141. 环形链表

---
+++ LeetCode/LeetCode/101-200/141-LinkedListCycle-E.swift
@@ -0,0 +1,66 @@
+//
+//  LinkedListCycle-E.swift
+//  LeetCode
+//
+//  Created by 张银龙 on 2020/2/23.
+//  Copyright © 2020 张银龙. All rights reserved.
+//
+
+import Cocoa
+/*
+ 141. 环形链表
+ 给定一个链表，判断链表中是否有环。
+
+ 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+  
+
+ 示例 1：
+
+ 输入：head = [3,2,0,-4], pos = 1
+ 输出：true
+ 解释：链表中有一个环，其尾部连接到第二个节点。
+
+
+ 示例 2：
+
+ 输入：head = [1,2], pos = 0
+ 输出：true
+ 解释：链表中有一个环，其尾部连接到第一个节点。
+
+
+ 示例 3：
+
+ 输入：head = [1], pos = -1
+ 输出：false
+ 解释：链表中没有环。
+
+ 进阶：
+
+ 你能用 O(1)（即，常量）内存解决此问题吗？
+ */
+class LinkedListCycle: NSObject {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        if head == nil {
+            return false
+        }
+        
+        
+        var fast = head
+        var slow = head
+        
+        
+        while fast != nil, fast?.next != nil {
+            
+            fast = fast?.next?.next
+            slow = slow?.next
+            
+            if fast == slow {
+                return true
+            }
+        }
+        
+        
+        return false
+    }
+}
--- LeetCode/LeetCode/Class/ListNode.swift
+++ LeetCode/LeetCode/Class/ListNode.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
- public class ListNode {
+public class ListNode: NSObject {
      public var val: Int
      public var next: ListNode?
      public init(_ val: Int) {
